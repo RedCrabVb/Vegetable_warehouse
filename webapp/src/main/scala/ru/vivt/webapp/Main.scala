7fd869e90b627d8c6277a85c87ec40ee680568ce
Alex <a.borodai@neoflex.ru>
2022-03-18 16:46:56 +0300

    html to scalajs

--- webapp/src/main/scala/ru/vivt/webapp/Main.scala
+++ webapp/src/main/scala/ru/vivt/webapp/Main.scala
@@ -5,26 +5,5 @@ import org.scalajs.dom.document
 import org.scalajs.dom.raw.Element
 import scalatags.JsDom.all._
 object Main extends App {
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = html(
-      head(
-        script("some script")
-      ),
-      body(
-        h1("This is my title"),
-        div(
-          p("This is my first paragraph"),
-          p("This is my second paragraph")
-        ),
-        button("test", (`class` := "btn btn-primary"))
-      )
-    ).render
 
-//    val parNode: Element = document.createElement("p")
-//    parNode.textContent = text
-    targetNode.appendChild(parNode)
-  }
-
-
-  appendPar(document.body, "Hello scalajs")
 }
---
+++ webapp/src/main/scala/ru/vivt/webapp/form/Authorization.scala
@@ -0,0 +1,100 @@
+package ru.vivt.webapp.form
+
+import org.scalajs.dom.document
+
+import scala.scalajs.js.annotation.JSExportTopLevel
+
+object Authorization {
+  def addForm(container: String, getBody: String => String) = {
+    val containerTag = document.getElementById(container)
+    if (containerTag != null) {
+      containerTag.innerHTML = getBody(container)
+    }
+  }
+  @JSExportTopLevel("addFormAuthorization")
+  def addFormAuthorization(container: String) = {
+    addForm(container, getBodyLoginIn(_))
+  }
+
+  @JSExportTopLevel("addFormInputUser")
+  def addFormInputUser(container: String) = {
+    addForm(container, getBodyInputUser(_))
+  }
+
+  @JSExportTopLevel("addFormEmployee")
+  def addFormEmployee(container: String, login: String, password: String) = {
+    addForm(container, getBodyEmployee(_))
+  }
+
+  @JSExportTopLevel("sendData")
+  def sendData(login: String = "", password: String = "", accountType: String = "") = {
+    println("sendData")
+    //send data on registration
+    //set status alert
+  }
+
+  def getBodyEmployee(container: String) = {
+    s"""
+       |<div form="$container">
+       |        <div class="mb-3">
+       |            <label for="exampleInputEmail1" class="form-label">Полное имя</label>
+       |            <input type="text" name="username" class="form-control"
+       |                   id="exampleInputEmail1"
+       |                   aria-describedby="emailHelp">
+       |        </div>
+       |        <div class="mb-3">
+       |            <label for="exampleInputPassword1" class="form-label">Данные паспорта</label>
+       |            <input type="password" name="password" class="form-control"
+       |                   id="exampleInputPassword1">
+       |        </div>
+       |        <select class="form-select mb-3" aria-label="Должность">
+       |            <option selected>Выбор должности</option>
+       |            <option value="1">Продовец</option>
+       |            <option value="2">Администратор</option>
+       |        </select>
+       |        <button class="btn btn-primary" onClick="sendData()">Отправить</button>
+       |    </div>
+       |""".stripMargin
+  }
+
+
+  def getBodyInputUser(container: String) = {
+    s"""
+       |<div id="$container">
+       |        <div class="mb-3">
+       |            <label for="exampleInputEmail1" class="form-label">Имя
+       |                пользователя</label>
+       |            <input type="text" name="username" class="form-control"
+       |                   id="exampleInputEmail1"
+       |                   aria-describedby="emailHelp">
+       |        </div>
+       |        <div class="mb-3">
+       |            <label for="exampleInputPassword1" class="form-label">Пароль</label>
+       |            <input type="password" name="password" class="form-control"
+       |                   id="exampleInputPassword1">
+       |        </div>
+       |        Я являюсь:
+       |        <input type="button" class="btn btn-primary" onclick="addFormEmployee('$container', 'test', 'test')" text="сотрудник">сотрудник</input>
+       |        <button class="btn btn-primary" onclick="sendData()">клиент</button>
+       |    </div>
+       |""".stripMargin
+  }
+
+  def getBodyLoginIn(container: String) = {
+  s"""    <form action="login" method="post" id="$container">
+       |        <div class="mb-3">
+       |            <label for="exampleInputEmail1" class="form-label">Имя пользователя</label>
+       |            <input type="text" name="username" class="form-control"
+       |                   id="exampleInputEmail1"
+       |                   aria-describedby="emailHelp">
+       |        </div>
+       |        <div class="mb-3">
+       |            <label for="exampleInputPassword1" class="form-label">Пароль</label>
+       |            <input type="password" name="password" class="form-control"
+       |                   id="exampleInputPassword1">
+       |        </div>
+       |        <div class="mb-3"><a href="#" onclick="addFormInputUser('$container')"">Зарегистрироваться</a></div>
+       |        <button type="submit" class="btn btn-primary">Отправить</button>
+       |    </form>""".stripMargin
+  }
+  }
