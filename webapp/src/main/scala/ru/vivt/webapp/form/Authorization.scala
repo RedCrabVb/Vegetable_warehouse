eee6595fed3fd027526c2e3b3ad962db8ac0d44e
Alex <a.borodai@neoflex.ru>
2022-03-18 22:34:47 +0300

    Minimal example

--- webapp/src/main/scala/ru/vivt/webapp/form/Authorization.scala
+++ webapp/src/main/scala/ru/vivt/webapp/form/Authorization.scala
@@ -1,8 +1,16 @@
 package ru.vivt.webapp.form
 
-import org.scalajs.dom.document
+import org.scalajs.dom.{document, html, window}
+import org.scalajs.dom
 
 import scala.scalajs.js.annotation.JSExportTopLevel
+import dom.ext.Ajax
+import io.circe.generic.auto._
+import io.circe.syntax._
+import io.circe.parser._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 object Authorization {
   def addForm(container: String, getBody: String => String) = {
@@ -12,8 +20,8 @@ object Authorization {
     }
   }
   @JSExportTopLevel("addFormAuthorization")
-  def addFormAuthorization(container: String) = {
-    addForm(container, getBodyLoginIn(_))
+  def addFormAuthorization(container: String, containerAlert: String) = {
+    addForm(container, getBodyLoginIn(_, containerAlert))
   }
 
   @JSExportTopLevel("addFormInputUser")
@@ -26,11 +34,23 @@ object Authorization {
     addForm(container, getBodyEmployee(_))
   }
 
-  @JSExportTopLevel("sendData")
-  def sendData(fields: Array[String]) = {
-    println("sendData")
-    //send data on registration
-    //set status alert
+  @JSExportTopLevel("authorization")
+  def authorization(login: String, password: String, alertContainer: String) = {
+    Ajax.post("/login", s"username=${getValueInput(login)}&password=${getValueInput(password)}")
+      .onComplete {
+        case Success(xhr) =>
+          println(xhr.responseText)
+          window.location.replace("/app/main.html")
+        case Failure(t) => {
+          if(alertContainer.nonEmpty) {
+            document.getElementById(alertContainer).innerHTML = ("Ошибка при авторизации")
+          }
+        }
+      }
+  }
+
+  def getValueInput(name: String): String = {
+    document.getElementById(name).asInstanceOf[html.Input].value
   }
 
   def getBodyEmployee(container: String) = {
@@ -80,21 +100,21 @@ object Authorization {
        |""".stripMargin
   }
 
-  def getBodyLoginIn(container: String) = {
-  s"""    <form action="/login" method="post" id="$container">
+  def getBodyLoginIn(container: String, containerAlert: String) = {
+  s"""    <div id="$container">
        |        <div class="mb-3">
        |            <label for="exampleInputEmail1" class="form-label">Имя пользователя</label>
-       |            <input type="text" name="username" class="form-control"
+       |            <input type="text" id="username-input" class="form-control"
        |                   id="exampleInputEmail1"
        |                   aria-describedby="emailHelp">
        |        </div>
        |        <div class="mb-3">
-       |            <label for="exampleInputPassword1" class="form-label">Пароль</label>
-       |            <input type="password" name="password" class="form-control"
+       |            <label for="exampleInputPassword" class="form-label">Пароль</label>
+       |            <input type="password" id="password-input" class="form-control"
        |                   id="exampleInputPassword1">
        |        </div>
        |        <div class="mb-3"><a href="#" onclick="addFormInputUser('$container')"">Зарегистрироваться</a></div>
-       |        <button type="submit" class="btn btn-primary">Отправить</button>
-       |    </form>""".stripMargin
+       |        <button type="submit" class="btn btn-primary" onclick="authorization('username-input', 'password-input', '$containerAlert')">Отправить</button>
+       |    </div>""".stripMargin
   }
   }
